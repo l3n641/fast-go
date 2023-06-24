5cf4d5ea9494f4d30e2251cbdc565ac72664ac9e
l3n641 <l3n641@192.168.10.11>
2023-06-24 13:59:53 +0800

    配置初始化 & 全局变量

---
+++ global/app.go
@@ -0,0 +1,13 @@
+package global
+
+import (
+	"fast-go/config"
+	"github.com/spf13/viper"
+)
+
+type Application struct {
+	ConfigViper *viper.Viper
+	Config      config.Configuration
+}
+
+var App = new(Application)
